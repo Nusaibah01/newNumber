54af5d58ce2cf9ba284955ca1442fd92ec4816e4
Nusaibah01 <charifnusaibah01@gmail.com>
2024-06-04 07:28:45 +0200

    nn

---
+++ code.js
@@ -0,0 +1,12 @@
+// newNumber
+let numbers = [4, 8, 12, 16, 20]
+let currentIndex = 0
+
+function newNumber(){
+    if(currentIndex < numbers.length -1){
+
+    }else{
+        currentIndex = 0
+    }
+}
+console.log(numbers)
